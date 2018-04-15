0652bbdb7b853306aa4a2cf289eeabd05fcda372
boogie666 <bbugarschi@gmail.com>
2018-04-15 23:05:35 +0300

    added new props

--- background.js
+++ background.js
@@ -4,16 +4,19 @@ const theme = {
   "images": {
     "headerURL": "header.png"
   },
-  
   "colors": {
-    "accentcolor": "#303642",
-    "textcolor": "#F5F6F7",
-    "toolbar": "#414A59",
-    "toolbar_text": "#F5F6F7",
-    "toolbar_field": "#F5F6F7",
-    "toolbar_field_text": "#303642"
+      "accentcolor": "#303641",
+      "textcolor": "rgba(255,255,255,0.9)",
+      "toolbar": "#383C4A",
+      "toolbar_text": "#C2CDDB",
+      "toolbar_field": "#404552",
+      "toolbar_field_text": "#fff",
+      "toolbar_field_border": "#252A33",
+      "tab_text": "#fff",
+      "toolbar_top_separator": "#252A33",
+      "toolbar_bottom_separator": "#252A33",
+      "toolbar_vertical_separator": "#252A33"
   }
-
 };
 
 browser.theme.update(theme);
